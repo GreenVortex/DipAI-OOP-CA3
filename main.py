baf7c73936b7d091d56942f5b5cd7bb33031c460
Jan Andersson <jan.andersson.ie@gmail.com>
2021-03-06 06:29:35 +0000

    Update
    + Created user interface for running different functions
    + Added clear function to wipe Moodle page
    + Created google drive request that saves a copy for offline use and uses regex to find DIVS with tooltip in them as they contain the metadata for the videos
    * Marked TODO

--- main.py
+++ main.py
@@ -14,16 +14,41 @@ directory_array.pop(0)  # From GIT and pycharm
 
 
 # print(directory_array) use for testing array
+def clear():
+    directorycounter = 1
+    for x in directory_array:
+        summary = ""
+        data = [{'type': 'num', 'section': 0, 'summary': '', 'summaryformat': 1, 'visible': 1, 'highlight': 0,
+                 'sectionformatoptions': [{'name': 'level', 'value': '1'}]}]
+        # Assign the correct summary
+        data[0]['summary'] = summary
+        # Set the correct section number
+        data[0]['section'] = directorycounter
+        # Write the data back to Moodle
+        sec_write = LocalUpdateSections(courseid, data)
 
 
 def updatelinks():
     directorycounter = 1
     for x in directory_array:
         # BS4 magic
-        htmlfile = open("wk"+str(directorycounter)+"/index.html", encoding="utf8")
+        htmlfile = open("wk" + str(directorycounter) + "/index.html", encoding="utf8")
         soup = BeautifulSoup(htmlfile, 'lxml')
         soup = soup.title
-        summary = '<a href="https://mikhail-cct.github.io/ooapp/wk' + str(directorycounter) + '/">' + soup.string + '</a> &nbsp ''<a href="' + WeekRecording[directorycounter] + '> Week ' + str(directorycounter) + ' video </a> ' + '&nbsp <a href="https://github.com/mikhail-cct/ca3-test/raw/master/wk'+ str(directorycounter) +'/wk'+ str(directorycounter) +'.pdf>PDF Lesson</a>'
+        # Building summary
+        title = '<h2>' + soup.string + '</h2>'
+        powerpoint = '<a href="https://mikhail-cct.github.io/ooapp/wk' + str(
+            directorycounter) + '/">' + '<img src="https://icon-library.net//images/powerpoint-icon-vector/powerpoint' \
+                                        '-icon-vector-29.jpg" width="50" /> <b><h3>Powerpoint</h3></b>' + '</a> '
+        video = '<a href="' + WeekRecording[directorycounter] + '"/><img src="https://icon-library.net//images' \
+                                                                '/recording-icon-png/recording-icon-png-25.jpg" ' \
+                                                                'width="50" /><b><h3>Recording</h3></b></a> '
+        pdf = '<a href="https://github.com/mikhail-cct/ca3-test/raw/master/wk{}/wk{}.pdf"><img ' \
+              'src="https://icon-library.net//images/pdf-icon-image/pdf-icon-image-27.jpg" width="50" ' \
+              '/><b><h3>PDF</h3></b></a>'.format(
+            str(directorycounter),
+            str(directorycounter))
+        summary = title + '<hr> ' + video + '<hr> ' + powerpoint + '<hr> ' + pdf
         data = [{'type': 'num', 'section': 0, 'summary': '', 'summaryformat': 1, 'visible': 1, 'highlight': 0,
                  'sectionformatoptions': [{'name': 'level', 'value': '1'}]}]
         # Assign the correct summary
@@ -39,4 +64,13 @@ def updatelinks():
 # main program
 courseid = "23"  # Course ID
 sec = LocalGetSections(courseid)
-updatelinks()
+while True:
+    command = input(">> ")
+    if command == "/gdrive":
+        getdrivelinks()
+    elif command == "/update":
+        updatelinks()
+    elif command == "/clear":
+        clear()
+    else:
+        print("Commands: /gdrive, /update, /clear")
--- videolibrary.py
+++ videolibrary.py
@@ -1,5 +1,10 @@
-WeekRecording = ["","","","","","","","",""]
-WeekRecording[1] = "FREE"
+from bs4 import BeautifulSoup
+import requests
+import requests
+import re
+
+# Array of google drive links
+WeekRecording = ["", "", "", "", "", "", "", "", ""]
 WeekRecording[1] = "https://drive.google.com/file/d/1vyPoSlUc5hcXajllDyaqMKvlJOiYxbNH/view?usp=sharing"
 WeekRecording[2] = "https://drive.google.com/file/d/1elgdm2482AMcARz_NUVTjg8KBPmoLTxj/view?usp=sharing"
 WeekRecording[3] = "https://drive.google.com/file/d/1_RgK_fcatlpGOSDn6yokgOEZAFxKmTlc/view?usp=sharing"
@@ -8,3 +13,23 @@ WeekRecording[5] = "https://drive.google.com/file/d/1Gx_QXD9kQFJNi9_oOlQN4Sa9Irz
 WeekRecording[6] = "https://drive.google.com/file/d/1nx7eMPpso7oXgU-KViMQZb48qUZahLru/view?usp=sharing"
 WeekRecording[7] = "https://drive.google.com/file/d/1hKgn7qnNlnd91_2YdzcFPwQvn5NeQS2A/view?usp=sharing"
 WeekRecording[8] = "https://drive.google.com/file/d/1rQM7k4oCRTxnAb8gzIGVCgTv1gVuT3Wq/view?usp=sharing"
+
+
+# BS4 magic
+def getdrivelinks():
+    # Send request for google drive page
+    gdriverequest = requests.get("https://drive.google.com/drive/folders/1pFHUrmpLv9gEJsvJYKxMdISuQuQsd_qX")
+    # Create a file and store google drive page with videos for offline use
+    gdrivewebsite = open("gdrive.html", "w")
+    gdrivewebsite.write(gdriverequest.text)
+    gdrivewebsite.close()
+    # Load the file with the Google drive page source
+    gdriveraw = open('gdrive.html')
+    # Let's make some tasty soup with it now
+    soup = BeautifulSoup(gdriveraw, 'lxml')
+    # Check for divs containing the data-tooltip as they contain the metadata for the individual videos
+    soup = soup.find_all('div', {'data-tooltip'})
+    # print the list of all the video names
+    print(soup)
+
+# TODO: Fix the issue with the regular expression and load video file names into array to use for summary payload
